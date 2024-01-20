940e0f6294c2accc29d14c45fb8eb9b28abd6380
Kiritouse <2496305505@qq.com>
2024-01-20 17:26:39 +0800

    完成了热门文章接口的VO优化

---
+++ KunJinKao-framework/src/main/java/com/KunJinKao/domain/vo/HotArticleVo.java
@@ -0,0 +1,16 @@
+package com.KunJinKao.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotArticle {
+    //实际返回的文章信息,只需要有这几个信息就行了
+    private Long id;
+    private String title;
+
+    private Long viewCount;
+}
