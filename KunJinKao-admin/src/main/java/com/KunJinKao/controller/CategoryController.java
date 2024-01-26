2f772b3ab8acfd619d285b1497f47ed6e267cff9
Kiritouse <2496305505@qq.com>
2024-01-26 16:21:24 +0800

    完成了后台Excel导出

--- KunJinKao-admin/src/main/java/com/KunJinKao/controller/CategoryController.java
+++ KunJinKao-admin/src/main/java/com/KunJinKao/controller/CategoryController.java
@@ -3,13 +3,19 @@ package com.KunJinKao.controller;
 import com.KunJinKao.domain.ResponseResult;
 import com.KunJinKao.domain.dto.CategoryDto;
 import com.KunJinKao.domain.entity.Category;
+import com.KunJinKao.domain.vo.ExcelCategoryVo;
 import com.KunJinKao.domain.vo.PageVo;
+import com.KunJinKao.enums.AppHttpCodeEnum;
 import com.KunJinKao.service.CategoryService;
 import com.KunJinKao.domain.vo.CategoryVo;
 import com.KunJinKao.utils.BeanCopyUtils;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.KunJinKao.utils.WebUtils;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -68,5 +74,30 @@ public class CategoryController {
     public ResponseResult edit(@RequestBody Category category){
         categoryService.updateById(category);
         return ResponseResult.okResult();
+    }
+
+    //---------------------------把分类数据写入到Excel并导出-----------------------------
+
+    @GetMapping("/export")
+    //注意返回值类型是void
+    public void export(HttpServletResponse response) {
+        try {
+            //设置下载文件的请求头，下载下来的Excel文件叫'分类.xlsx'
+            WebUtils.setDownLoadHeader("分类.xlsx", response);
+            //获取需要导出的数据
+            List<Category> xxcategory = categoryService.list();
+
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(xxcategory, ExcelCategoryVo.class);
+            //把数据写入到Excel中，也就是把ExcelCategoryVo实体类的字段作为Excel表格的列头
+            //sheet方法里面的字符串是Excel表格左下角工作簿的名字
+            EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("文章分类")
+                    .doWrite(excelCategoryVos);
+
+        } catch (Exception e) {
+            //如果出现异常,就返回失败的json数据给前端。AppHttpCodeEnum和ResponseResult是我们在huanf-framework工程写的类
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            //WebUtils是我们在huanf-framework工程写的类，里面的renderString方法是将json字符串写入到请求体，然后返回给前端
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
     }
 }
--- KunJinKao-framework/src/main/java/com/KunJinKao/utils/WebUtils.java
+++ KunJinKao-framework/src/main/java/com/KunJinKao/utils/WebUtils.java
@@ -1,22 +1,23 @@
 package com.KunJinKao.utils;
 
-import org.springframework.web.context.request.RequestContextHolder;
-
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-public class WebUtils
-{
+/**
+ * @author 35238
+ * @date 2023/7/22 0022 21:19
+ */
+public class WebUtils {
     /**
      * 将字符串渲染到客户端
-     * 
+     *
      * @param response 渲染对象
      * @param string 待渲染的字符串
      * @return null
      */
+    //easyExcel文件导出失败时候做的操作
     public static void renderString(HttpServletResponse response, String string) {
         try
         {
@@ -28,18 +29,14 @@ public class WebUtils
         catch (IOException e)
         {
             e.printStackTrace();
-
         }
     }
 
-
-    public static void setDownLoadHeader(String filename, ServletContext context, HttpServletResponse response) throws UnsupportedEncodingException {
-        String mimeType = context.getMimeType(filename);//获取文件的mime类型
-        response.setHeader("content-type",mimeType);
-        String fname= URLEncoder.encode(filename,"UTF-8");
+    //easyExcel文件导出
+    public static void setDownLoadHeader(String filename, HttpServletResponse response) throws UnsupportedEncodingException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        String fname= URLEncoder.encode(filename,"UTF-8").replaceAll("\\+", "%20");
         response.setHeader("Content-disposition","attachment; filename="+fname);
-
-//        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-//        response.setCharacterEncoding("utf-8");
     }
 }
